8a481271be6008ca1b4b89f5e8da8586e2c9eaff
Igor Julliano <igor.julliano2@gmail.com>
2023-04-28 02:44:22 -0300

    feat: add custom theme with custom colors

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { ThemeProvider } from '@mui/material';
+import { customTheme } from './core/ui/theme';
 import { router } from './routes';
 import { RouterProvider } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <ThemeProvider theme={customTheme}>
       <RouterProvider router={router} />
+    </ThemeProvider>
   </React.StrictMode>
 );
