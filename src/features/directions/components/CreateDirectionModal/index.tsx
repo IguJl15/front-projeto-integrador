ca688a49c393e676581bab076133d9b3b14635e7
Igor julliano <igor.julliano2@gmail.com>
2023-07-02 00:23:45 -0300

    feat: add verification while adding terms

--- src/features/directions/components/CreateDirectionModal/index.tsx
+++ src/features/directions/components/CreateDirectionModal/index.tsx
@@ -5,230 +5,218 @@ import { Add } from '@mui/icons-material';
 import { FormRow, FormRowWithTextField } from '../TextField';
 
 export const CreateDirectionModal = () => {
-   // use errors
-   // TODO: ADD SET ERRORS
+  // use errors
+  // TODO: ADD SET ERRORS
 
-   // input fields
-   const [title, setTitle] = useState('');
-   const [email, setEmail] = useState('');
-   const [incTerm, setIncTerm] = useState('');
-   const [excTerm, setExcTerm] = useState('');
+  // input fields
+  const [title, setTitle] = useState('');
+  const [email, setEmail] = useState('');
+  const [incTerm, setIncTerm] = useState('');
+  const [excTerm, setExcTerm] = useState('');
 
-   // terms
-   const [inclusionTerms, setInclusionTerms] = useState(new Set<string>());
-   const [exclusionterms, setExclusionTerms] = useState(new Set<string>());
+  // terms
+  const [inclusionTerms, setInclusionTerms] = useState(new Set<string>());
+  const [exclusionterms, setExclusionTerms] = useState(new Set<string>());
 
-   // modal state
-   const [open, setOpen] = useState(false);
-   const handleOpen = () => setOpen(true);
-   const handleClose = () => setOpen(false);
+  // modal state
+  const [open, setOpen] = useState(false);
+  // modal function
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => {
+    resetModal();
+    setOpen(false);
+  };
 
-   function createDirection() {
-      try {
-         // create direction
+  function createDirection() {
+    try {
+      // create direction
 
-         resetModal();
-         handleClose();
-      } catch (error) {
-         if (error instanceof DirectionError) {
-            // show errors
-         }
+      handleClose();
+    } catch (error) {
+      // if (error instanceof DirectionError) {
+      //    // show errors
+      // }
+    }
+  }
+
+  function resetModal() {
+    setTitle('');
+    setEmail('');
+    setIncTerm('');
+    setExcTerm('');
+
+    setInclusionTerms(new Set());
+    setExclusionTerms(new Set());
+  }
+
+  function addIncTerm() {
+    if (incTerm && incTerm.trim()) {
+      if (exclusionterms.has(incTerm)) {
+        // lancar error
+        return;
       }
-   }
+      setInclusionTerms(inclusionTerms.add(incTerm));
+    }
+  }
+  function addExcTerm() {
+    if (excTerm && excTerm.trim()) {
+      if (inclusionTerms.has(excTerm)) {
+        // lancar error
+        return;
+      }
+      setExclusionTerms(exclusionterms.add(excTerm));
+    }
+  }
 
-   function resetModal() {
-      setTitle('');
-      setEmail('');
-      setIncTerm('');
-      setExcTerm('');
-
-      setInclusionTerms(new Set());
-      setExclusionTerms(new Set());
-   }
-
-   function addIncTerm(term: string) {
-      if (term && term.trim()) setInclusionTerms(inclusionTerms.add(term));
-   }
-   function addExcTerm(term: string) {
-      if (term && term.trim()) setExclusionTerms(exclusionterms.add(term));
-   }
-
-   return (
-      <>
-         <Button onClick={handleOpen} variant="outlined">
-            Open modal
-         </Button>
-         <Modal
-            open={open}
-            onClose={handleClose}
-            aria-labelledby="modal-modal-title"
-            aria-describedby="modal-modal-description"
-         >
-            <div className={styles.container}>
-               <Stack gap={1} width={'100%'}>
-                  <div className={styles.modal_title}>
-                     <Typography
-                        id="modal-modal-title"
-                        variant="h4"
-                        component="h2"
-                     >
-                        Criar Direcionamento
-                     </Typography>
-                  </div>
-                  <Divider variant="fullWidth" />
-                  <div className={styles.modal_information_frame}>
-                     <div className={styles.modal_information_title}>
-                        <span>Informacoes</span>
-                     </div>
-                     <div className={styles.modal_information_form}>
-                        <FormRowWithTextField
-                           inputLabel="Titulo"
-                           inputPlaceholder="Insira seu Titulo"
-                           inputName="title"
-                           inputType="text"
-                           value={title}
-                           onChange={(event: ChangeEvent<HTMLInputElement>) => {
-                              setTitle(event.target.value);
-                           }}
-                        />
-                        <FormRowWithTextField
-                           inputLabel="Email para direcionamento"
-                           inputPlaceholder="botnews@gmail.com"
-                           inputName="email"
-                           inputType="email"
-                           value={email}
-                           onChange={(event: ChangeEvent<HTMLInputElement>) => {
-                              setEmail(event.target.value);
-                           }}
-                        />
-                     </div>
-                  </div>
-                  <Divider variant="fullWidth" />
-                  <div className={styles.modal_filter_frame}>
-                     <div className={styles.modal_filter_typography}>
-                        <span>Termos</span>
-                     </div>
-                     <Stack
-                        direction="column"
-                        spacing={1}
-                        pt={1}
-                        sx={{ flex: 1 }}
-                     >
-                        {/* TERMOS DE INCLUSÃO */}
-                        <span className={styles.modal_filter_text_area}>
-                           Termos de inclusao
-                        </span>
-                        <FormRowWithTextField
-                           inputLabel="Novo termo"
-                           inputName="term name"
-                           inputType="text"
-                           inputPlaceholder="Insira seu termo"
-                           value={incTerm}
-                           onChange={(
-                              event: React.ChangeEvent<HTMLInputElement>
-                           ) => {
-                              setIncTerm(event.target.value);
-                           }}
-                           trailingButton={
-                              <Button
-                                 type="submit"
-                                 variant="outlined"
-                                 startIcon={<Add />}
-                                 onClick={(
-                                    event: React.MouseEvent<HTMLButtonElement>
-                                 ) => {
-                                    event.preventDefault();
-                                    addIncTerm(incTerm);
-                                    setIncTerm('');
-                                 }}
-                              >
-                                 Adicionar
-                              </Button>
-                           }
-                        />
-                        <FormRow label="Termos adicionados" name="new_terms">
-                           <div className={styles.termsList}>
-                              {Array.from(inclusionTerms.values()).map(
-                                 (term) => {
-                                    return (
-                                       <div className={styles.term}>
-                                          <Typography
-                                             variant="body2"
-                                             fontWeight={500}
-                                          >
-                                             {term.toUpperCase()}
-                                          </Typography>
-                                       </div>
-                                    );
-                                 }
-                              )}
-                           </div>
-                        </FormRow>
-
-                        {/* TERMOS DE EXCLUSÃO */}
-                        <span className={styles.modal_filter_text_area}>
-                           Termos de exclusão
-                        </span>
-                        <FormRowWithTextField
-                           inputLabel="Novo termo"
-                           inputName="term name"
-                           inputType="text"
-                           inputPlaceholder="Novo termo de exclusão"
-                           value={excTerm}
-                           onChange={(
-                              event: React.ChangeEvent<HTMLInputElement>
-                           ) => {
-                              setExcTerm(event.target.value);
-                           }}
-                           trailingButton={
-                              <Button
-                                 type="submit"
-                                 variant="outlined"
-                                 startIcon={<Add />}
-                                 onClick={(
-                                    event: React.MouseEvent<HTMLButtonElement>
-                                 ) => {
-                                    event.preventDefault();
-                                    addExcTerm(excTerm);
-                                    setExcTerm('');
-                                 }}
-                              >
-                                 Adicionar
-                              </Button>
-                           }
-                        />
-                        <FormRow label="Termos adicionados" name="bad_terms">
-                           <div className={styles.termsList}>
-                              {Array.from(exclusionterms.values()).map(
-                                 (term) => {
-                                    return (
-                                       <div className={styles.term}>
-                                          <Typography
-                                             variant="body2"
-                                             fontWeight={500}
-                                          >
-                                             {term.toUpperCase()}
-                                          </Typography>
-                                       </div>
-                                    );
-                                 }
-                              )}
-                           </div>
-                        </FormRow>
-                     </Stack>
-                  </div>
-                  <Divider variant="fullWidth" />
-                  <footer className="footer">
-                     <Button onClick={handleClose} variant="outlined">
-                        CANCELAR
-                     </Button>
-                     <Button onClick={createDirection} variant="contained">
-                        CRIAR DIRECIONAMENTO
-                     </Button>
-                  </footer>
-               </Stack>
+  return (
+    <>
+      <Button onClick={handleOpen} variant="outlined">
+        Open modal
+      </Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <div className={styles.container}>
+          <Stack gap={1} width={'100%'}>
+            <div className={styles.modal_title}>
+              <Typography id="modal-modal-title" variant="h4" component="h2">
+                Criar Direcionamento
+              </Typography>
             </div>
-         </Modal>
-      </>
-   );
+            <Divider variant="fullWidth" />
+            <div className={styles.modal_information_frame}>
+              <div className={styles.modal_information_title}>
+                <span>Informacoes</span>
+              </div>
+              <div className={styles.modal_information_form}>
+                <FormRowWithTextField
+                  inputLabel="Titulo"
+                  inputPlaceholder="Insira seu Titulo"
+                  inputName="title"
+                  inputType="text"
+                  value={title}
+                  onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                    setTitle(event.target.value.toUpperCase());
+                  }}
+                />
+                <FormRowWithTextField
+                  inputLabel="Email para direcionamento"
+                  inputPlaceholder="botnews@gmail.com"
+                  inputName="email"
+                  inputType="email"
+                  value={email}
+                  onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                    setEmail(event.target.value.toUpperCase());
+                  }}
+                />
+              </div>
+            </div>
+            <Divider variant="fullWidth" />
+            <div className={styles.modal_filter_frame}>
+              <div className={styles.modal_filter_typography}>
+                <span>Termos</span>
+              </div>
+              <Stack direction="column" spacing={1} pt={1} sx={{ flex: 1 }}>
+                {/* TERMOS DE INCLUSÃO */}
+                <span className={styles.modal_filter_text_area}>
+                  Termos de inclusao
+                </span>
+                <FormRowWithTextField
+                  inputLabel="Novo termo"
+                  inputName="term name"
+                  inputType="text"
+                  inputPlaceholder="Insira seu termo"
+                  value={incTerm}
+                  onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                    setIncTerm(event.target.value.toUpperCase());
+                  }}
+                  trailingButton={
+                    <Button
+                      type="submit"
+                      variant="outlined"
+                      startIcon={<Add />}
+                      onClick={(event: React.MouseEvent<HTMLButtonElement>) => {
+                        event.preventDefault();
+                        addIncTerm();
+                        setIncTerm('');
+                      }}
+                    >
+                      Adicionar
+                    </Button>
+                  }
+                />
+                <FormRow label="Termos adicionados" name="new_terms">
+                  <div className={styles.termsList}>
+                    {Array.from(inclusionTerms.values()).map((term) => {
+                      return (
+                        <div className={styles.term}>
+                          <Typography variant="body2" fontWeight={500}>
+                            {term}
+                          </Typography>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </FormRow>
+
+                {/* TERMOS DE EXCLUSÃO */}
+                <span className={styles.modal_filter_text_area}>
+                  Termos de exclusão
+                </span>
+                <FormRowWithTextField
+                  inputLabel="Novo termo"
+                  inputName="term name"
+                  inputType="text"
+                  inputPlaceholder="Novo termo de exclusão"
+                  value={excTerm}
+                  onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                    setExcTerm(event.target.value.toUpperCase());
+                  }}
+                  trailingButton={
+                    <Button
+                      type="submit"
+                      variant="outlined"
+                      startIcon={<Add />}
+                      onClick={(event: React.MouseEvent<HTMLButtonElement>) => {
+                        event.preventDefault();
+                        addExcTerm();
+                        setExcTerm('');
+                      }}
+                    >
+                      Adicionar
+                    </Button>
+                  }
+                />
+                <FormRow label="Termos adicionados" name="bad_terms">
+                  <div className={styles.termsList}>
+                    {Array.from(exclusionterms.values()).map((term) => {
+                      return (
+                        <div className={styles.term}>
+                          <Typography variant="body2" fontWeight={500}>
+                            {term.toUpperCase()}
+                          </Typography>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </FormRow>
+              </Stack>
+            </div>
+            <Divider variant="fullWidth" />
+            <footer className="footer">
+              <Button onClick={handleClose} variant="outlined">
+                CANCELAR
+              </Button>
+              <Button onClick={createDirection} variant="contained">
+                CRIAR DIRECIONAMENTO
+              </Button>
+            </footer>
+          </Stack>
+        </div>
+      </Modal>
+    </>
+  );
 };
--- src/features/directions/components/TextField/index.tsx
+++ src/features/directions/components/TextField/index.tsx
@@ -1,48 +1,48 @@
 import { TextField, Typography } from '@mui/material';
 import styles from './style.module.css';
-import { Children, ReactElement, ReactNode } from 'react';
+import { ReactNode } from 'react';
 
 interface IRowWithTextFieldProps {
-   inputName: string;
-   inputLabel: string;
-   inputPlaceholder: string;
-   inputType?: React.HTMLInputTypeAttribute;
-   value: string;
-   onChange: (event: React.ChangeEvent<HTMLInputElement>) => any;
-   trailingButton?: ReactNode;
+  inputName: string;
+  inputLabel: string;
+  inputPlaceholder: string;
+  inputType?: React.HTMLInputTypeAttribute;
+  value: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => any;
+  trailingButton?: ReactNode;
 }
 
 export const FormRowWithTextField = (props: IRowWithTextFieldProps) => {
-   return (
-      <form>
-         <FormRow name={props.inputName} label={props.inputLabel}>
-            <TextField
-               name={props.inputName}
-               placeholder={props.inputPlaceholder}
-               type={props.inputType}
-               value={props.value}
-               size="small"
-               onChange={props.onChange}
-            />
-            {props.trailingButton}
-         </FormRow>
-      </form>
-   );
+  return (
+    <form>
+      <FormRow name={props.inputName} label={props.inputLabel}>
+        <TextField
+          name={props.inputName}
+          placeholder={props.inputPlaceholder}
+          type={props.inputType}
+          value={props.value}
+          size="small"
+          onChange={props.onChange}
+        />
+        {props.trailingButton}
+      </FormRow>
+    </form>
+  );
 };
 
 interface IFormRowProps {
-   name: string;
-   label: string;
-   children: ReactNode | ReactNode[];
+  name: string;
+  label: string;
+  children: ReactNode | ReactNode[];
 }
 
 export const FormRow = (props: IFormRowProps) => {
-   return (
-      <div className={styles.formRow}>
-         <label htmlFor={props.name}>
-            <Typography>{props.label}</Typography>
-         </label>
-         {props.children}
-      </div>
-   );
+  return (
+    <div className={styles.formRow}>
+      <label htmlFor={props.name}>
+        <Typography>{props.label}</Typography>
+      </label>
+      {props.children}
+    </div>
+  );
 };
